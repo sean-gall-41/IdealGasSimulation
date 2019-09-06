5d0c93ff6f99a70e69f76606abd4a80d9cdae0dc
sean-gall-41 <galloglyst149@gmail.com>
2019-09-05 20:43:32 -0500

    removing commented out code, other minor tweaks

--- ideal_gas.py
+++ ideal_gas.py
@@ -55,12 +55,7 @@ class GasParticle:
         self.__velocity = velocity
 
     # bounds is a list rep the bounding box: [x_min, x_max, y_min, y_max]
-    # TODO: fix wacky bouncing bug for off values/combinations of scale factor and radius
-    #       Maybe particle is getting trapped within the distance calculated between frames?
-    def step(self, dt, bounds):
-        # TODO: Should probably make delta dependent on radius so that changing global var doesnt 
-        #       mess everything up. A fair assessment: if we increase radius, we get a greater "window"
-        #       for our particle to become trapped in. 
+    def step(self, dt, bounds): 
 
         if self.position[0] < self.radius + bounds[0]:
             self.position[0] = self.radius + bounds[0]
@@ -75,23 +70,10 @@ class GasParticle:
             self.position[1] = -self.radius + bounds[3]
             self.velocity[1] *= -1
 
-
-        # delta = 1.0E-2
-        # epsilon = self.radius + delta
-        # if abs(bounds[1]-self.position[0]) <= epsilon:
-        #     self.velocity[0] = -self.velocity[0]
-        # elif abs(bounds[0]-self.position[0]) <= epsilon:
-        #     self.velocity[0] = -self.velocity[0]
-        # elif abs(bounds[3]-self.position[1]) <= epsilon:
-        #     self.velocity[1] = -self.velocity[1]
-        # elif abs(bounds[2]-self.position[1]) <= epsilon:
-        #     self.velocity[1] = -self.velocity[1]
-        #now update positions regardless if collision occurred 
         self.position += dt*self.velocity
 
 # Set up figure
 fig = plt.figure()
-# fig.subplots_adjust(left=0, right=1, bottom=0, top=1) #Kind of unsure what this does?
 ax = fig.add_subplot(111, aspect='equal', autoscale_on=False,
                       xlim=(X_LOWER, X_UPPER), ylim=(Y_LOWER, Y_UPPER))
 
@@ -105,30 +87,30 @@ ax.add_patch(rect)
 #TODO: make sure particle trails fade away over time
 my_particle = GasParticle(PARTICLE_MASS, PARTICLE_RADIUS,
                           [-0.5*BOX_WIDTH, 0.0],
-                          [5.0, 2.5])
+                          [2.0, 2.0])
 
 #TODO: find correct conversion factor for radius to marker size (in points)
 marker_size = int(fig.dpi*2*my_particle.radius*fig.get_figwidth()
                   / np.diff(ax.get_xbound())[0])
 particle_pos, = ax.plot([], [], 'bo', ms=marker_size)
-pos_x = []
-pos_y = []
-particle_path, = ax.plot([], [], c='0.75', lw=1)
+# pos_x = []
+# pos_y = []
+# particle_path, = ax.plot([], [], c='0.75', lw=1)
 
 dt = 1./30
 
 def init():
     particle_pos.set_data([], [])
-    particle_path.set_data(pos_x, pos_y)
-    return particle_pos, particle_path,
+    # particle_path.set_data(pos_x, pos_y)
+    return particle_pos, #particle_path,
 
 def animate(i):
     my_particle.step(dt, [-0.5*BOX_WIDTH, 0.5*BOX_WIDTH, -0.5*BOX_HEIGHT, 0.5*BOX_HEIGHT])
-    pos_x.append(my_particle.position[0])
-    pos_y.append(my_particle.position[1])
+    # pos_x.append(my_particle.position[0])
+    # pos_y.append(my_particle.position[1])
     particle_pos.set_data([my_particle.position[0]], [my_particle.position[1]])
-    particle_path.set_data(pos_x, pos_y)
-    return particle_pos, particle_path,
+    # particle_path.set_data(pos_x, pos_y)
+    return particle_pos, #particle_path,
 
 anim = animation.FuncAnimation(fig, animate, frames=30, interval=10, blit=True,
                                init_func=init)
