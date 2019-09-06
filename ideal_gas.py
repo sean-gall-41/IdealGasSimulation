37f1bfc46ae40d617e2224cd4eef023b6c75f2d0
sean-gall-41 <galloglyst149@gmail.com>
2019-09-06 13:43:24 -0500

    Update ideal_gas.py
    merging bounceboxbugfix with master

--- ideal_gas.py
+++ ideal_gas.py
@@ -5,7 +5,7 @@ import matplotlib.animation as animation
 #GLOBAL VARIABLES
 
 X_LOWER, X_UPPER, Y_LOWER, Y_UPPER = -5.0, 5.0, -5.0, 5.0
-PARTICLE_MASS, PARTICLE_RADIUS = 0.01, 0.05
+PARTICLE_MASS, PARTICLE_RADIUS = 0.01, 0.07
 SCALE_FACTOR = 0.5
 BOX_WIDTH = SCALE_FACTOR*(X_UPPER-X_LOWER)
 BOX_HEIGHT = SCALE_FACTOR*(Y_UPPER-Y_LOWER)
@@ -55,23 +55,63 @@ class GasParticle:
         self.__velocity = velocity
 
     # bounds is a list rep the bounding box: [x_min, x_max, y_min, y_max]
-    # TODO: fix wacky bouncing bug for off values/combinations of scale factor and radius
-    #       Maybe particle is getting trapped within the distance calculated between frames?
-    def step(self, dt, bounds):
-        if abs(bounds[1]-self.position[0]) < self.radius:
-            self.velocity[0] = -self.velocity[0]
-        elif abs(bounds[0]-self.position[0]) < self.radius:
-            self.velocity[0] = -self.velocity[0]
-        elif abs(bounds[3]-self.position[1]) < self.radius:
-            self.velocity[1] = -self.velocity[1]
-        elif abs(bounds[2]-self.position[1]) < self.radius:
-            self.velocity[1] = -self.velocity[1]
-        #now update positions regardless if collision occurred 
+    def step(self, dt, bounds): 
+
+        if self.position[0] < self.radius + bounds[0]:
+            self.position[0] = self.radius + bounds[0]
+            self.velocity[0] *= -1
+        if self.position[0] > -self.radius + bounds[1]:
+            self.position[0] = -self.radius + bounds[1]
+            self.velocity[0] *= -1
+        if self.position[1] < self.radius + bounds[2]:
+            self.position[1] = self.radius + bounds[2]
+            self.velocity[1] *= -1
+        if self.position[1] > -self.radius + bounds[3]:
+            self.position[1] = -self.radius + bounds[3]
+            self.velocity[1] *= -1
+
         self.position += dt*self.velocity
 
+# A particle box represents a group of N gas particles confined
+# within a box of dimensions bounds
+class ParticleBox():
+    def __init__(self, N=25, bounds=[-1.0, 1.0, -1.0, 1.0]):
+        self.__N = N
+        self.__bounds = bounds
+        self.__particle_list = np.array([GasParticle(PARTICLE_MASS, PARTICLE_RADIUS,
+                                        [-0.5*BOX_WIDTH + BOX_WIDTH*np.random.random(), 
+                                        -0.5*BOX_HEIGHT + BOX_HEIGHT*np.random.random()],
+                                        -0.5 + np.random.random((1,2))) for i in np.arange(N)])
+
+    @property
+    def N(self):
+        return self.__N
+
+    @N.setter
+    def N(self, N):
+        self.__N = N
+
+    @property 
+    def bounds(self):
+        return self.__bounds
+
+    @bounds.setter
+    def bounds(self, bounds):
+        self.__bounds = bounds
+    
+    @property
+    def particle_list(self):
+        return self.__particle_list
+
+    # TODO: should probably run some kind of input validation on this...
+    @particle_list.setter
+    def particle_list(self, list):
+        self.particle_list = list
+
+    
+my_box = ParticleBox(20, [-0.5*BOX_WIDTH, 0.5*BOX_WIDTH, -0.5*BOX_HEIGHT, 0.5*BOX_HEIGHT])
 # Set up figure
 fig = plt.figure()
-# fig.subplots_adjust(left=0, right=1, bottom=0, top=1) #Kind of unsure what this does?
 ax = fig.add_subplot(111, aspect='equal', autoscale_on=False,
                       xlim=(X_LOWER, X_UPPER), ylim=(Y_LOWER, Y_UPPER))
 
@@ -83,32 +123,40 @@ rect = plt.Rectangle((-0.5*BOX_WIDTH, -0.5*BOX_HEIGHT), BOX_WIDTH,
 ax.add_patch(rect)
 
 #TODO: make sure particle trails fade away over time
-my_particle = GasParticle(PARTICLE_MASS, PARTICLE_RADIUS,
-                          [-0.5*BOX_WIDTH+0.5, -0.5*BOX_HEIGHT+0.5],
-                          [3.2, 2.0])
+
+np.random.seed(0)
+# particles = [GastParticle(PARTICLE_MASS, PARTICLE_RADIUS, )]
+
+my_particle_1 = GasParticle(PARTICLE_MASS, PARTICLE_RADIUS,
+                          [-0.5*BOX_WIDTH, 0.0],
+                          [2.0, 2.0])
+my_particle_2 = GasParticle(PARTICLE_MASS, PARTICLE_RADIUS,
+                            [0.5*BOX_WIDTH, 0.0], [-2.0, 3.0])
 
 #TODO: find correct conversion factor for radius to marker size (in points)
-marker_size = int(fig.dpi*2*my_particle.radius*fig.get_figwidth()
+marker_size = int(fig.dpi*2*my_particle_1.radius*fig.get_figwidth()
                   / np.diff(ax.get_xbound())[0])
 particle_pos, = ax.plot([], [], 'bo', ms=marker_size)
-pos_x = []
-pos_y = []
-particle_path, = ax.plot([], [], 'g--', lw=2)
+# pos_x = []
+# pos_y = []
+# particle_path, = ax.plot([], [], c='0.75', lw=1)
 
 dt = 1./30
 
 def init():
     particle_pos.set_data([], [])
-    particle_path.set_data(pos_x, pos_y)
-    return particle_pos, particle_path,
+    # particle_path.set_data(pos_x, pos_y)
+    return particle_pos, #particle_path,
 
 def animate(i):
-    my_particle.step(dt, [-0.5*BOX_WIDTH, 0.5*BOX_WIDTH, -0.5*BOX_HEIGHT, 0.5*BOX_HEIGHT])
-    pos_x.append(my_particle.position[0])
-    pos_y.append(my_particle.position[1])
-    particle_pos.set_data([my_particle.position[0]], [my_particle.position[1]])
-    particle_path.set_data(pos_x, pos_y)
-    return particle_pos, particle_path,
+    my_particle_1.step(dt, [-0.5*BOX_WIDTH, 0.5*BOX_WIDTH, -0.5*BOX_HEIGHT, 0.5*BOX_HEIGHT])
+    my_particle_2.step(dt, [-0.5*BOX_WIDTH, 0.5*BOX_WIDTH, -0.5*BOX_HEIGHT, 0.5*BOX_HEIGHT])
+    # pos_x.append(my_particle.position[0])
+    # pos_y.append(my_particle.position[1])
+    particle_pos.set_data([my_particle_1.position[0], my_particle_2.position[0]], 
+                          [my_particle_1.position[1], my_particle_2.position[1]])
+    # particle_path.set_data(pos_x, pos_y)
+    return particle_pos, #particle_path,
 
 anim = animation.FuncAnimation(fig, animate, frames=30, interval=10, blit=True,
                                init_func=init)
