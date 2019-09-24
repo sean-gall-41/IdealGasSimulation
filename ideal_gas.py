1e7d24ebbf8023e6dc1fa0c95e0896852373defb
sean-gall-41 <galloglyst149@gmail.com>
2019-09-23 23:33:14 -0500

    converted position and velocity into one state numpy array

--- ideal_gas.py
+++ ideal_gas.py
@@ -21,104 +21,70 @@ class GasParticle:
     """To understand how we are going to initialize our ball object, 
     we need to understand how to plot artists in matplotlib"""
     def __init__(self, mass = PARTICLE_MASS, radius = PARTICLE_RADIUS,
-                 position = [0.0, 0.0], velocity = [0.0, 0.0]):
-        self.__mass = mass
-        self.__radius = radius
-        self.__position = np.asarray(position, dtype=float)
-        self.__velocity = np.asarray(velocity, dtype=float)
+                 init_state = [0.0, 0.0, 0.0, 0.0]):
+        self.mass = mass
+        self.radius = radius
+        self.init_state = np.asarray(init_state, dtype=float)
+        self.state = self.init_state.copy()
 
-    @property
-    def mass(self):
-        return self.__mass
+    # @property
+    # def mass(self):
+    #     return self.__mass
 
-    @mass.setter
-    def mass(self, mass):
-        self.__mass = mass
+    # @mass.setter
+    # def mass(self, mass):
+    #     self.__mass = mass
     
-    @property
-    def radius(self):
-        return self.__radius
+    # @property
+    # def radius(self):
+    #     return self.__radius
 
-    @radius.setter
-    def radius(self, radius):
-        self.__radius = radius
+    # @radius.setter
+    # def radius(self, radius):
+    #     self.__radius = radius
 
-    @property
-    def position(self):
-        return self.__position
+    # @property
+    # def position(self):
+    #     return self.__position
 
-    @position.setter
-    def position(self, position):
-        self.__position = position
+    # @position.setter
+    # def position(self, position):
+    #     self.__position = position
 
-    @property
-    def velocity(self):
-        return self.__velocity
+    # @property
+    # def velocity(self):
+    #     return self.__velocity
 
-    @velocity.setter
-    def velocity(self, velocity):
-        self.__velocity = velocity
+    # @velocity.setter
+    # def velocity(self, velocity):
+    #     self.__velocity = velocity
+
+    def __eq__(self, other):
+        return True if self.mass == other.mass and self.radius == other.radius and np.array_equal(self.position, other.position) and np.array_equal(self.velocity and other.velocity) else False
 
     # predicate function to determine whether the caller collides with
     # the parameter particle
-    def collides_with(self, particle):
-        D =  np.sqrt((self.position[0] - particle.position[0])**2 + 
-                     (self.position[1] - particle.position[1])**2)
+    def collides_with(self, other):
+        D =  np.sqrt((self.state[0] - other.state[0])**2 + 
+                     (self.state[1] - other.state[1])**2)
         
-        return True if D < self.radius + particle.radius else False
+        return True if D < self.radius + other.radius else False
 
 
 # A particle box represents a group of N gas particles confined
 # within a box of dimensions bounds
 class ParticleBox():
     def __init__(self, N=25, bounds=[-1.0, 1.0, -1.0, 1.0]):
-        self.__t = 0.0
-        self.__N = N
-        self.__bounds = bounds
-        self.__particle_list = np.array([GasParticle(PARTICLE_MASS, PARTICLE_RADIUS,
+        self.t = 0.0
+        self.N = N
+        self.bounds = bounds
+        self.particle_list = np.array([GasParticle(PARTICLE_MASS, PARTICLE_RADIUS,
                                         [-0.5*BOX_WIDTH + BOX_WIDTH*np.random.random(), 
-                                        -0.5*BOX_HEIGHT + BOX_HEIGHT*np.random.random()],
-                                        [-1.0 + np.random.random(), -1.0 + np.random.random()]) 
+                                        -0.5*BOX_HEIGHT + BOX_HEIGHT*np.random.random(),
+                                        -1.0 + np.random.random(), -1.0 + np.random.random()]) 
                                         for i in np.arange(N)])
 
-    @property
-    def t(self):
-        return self.__t
-    
-    @t.setter
-    def t(self, t):
-        self.__t = t
-    
-    @property
-    def N(self):
-        return self.__N
 
-    @N.setter
-    def N(self, N):
-        self.__N = N
-
-    @property 
-    def bounds(self):
-        return self.__bounds
-
-    @bounds.setter
-    def bounds(self, bounds):
-        self.__bounds = bounds
-    
-    @property
-    def particle_list(self):
-        return self.__particle_list
-
-    # TODO: should probably run some kind of input validation on this...
-    @particle_list.setter
-    def particle_list(self, list):
-        self.particle_list = list
-
-    # TODO: migrate step function from GasParticle class over here. Then
-    # have step return the updated list of positions and velocities.
-    # This also means you should combine position and velocity lists into 
-    # a single list, for easier readability and more maintainable code (like
-    # when you access positions in animate function: just use list-splicing)
     # Steps the state of the particle box forward by time dt
     # => all particle positions and velocities are updated. 
     
@@ -126,22 +92,25 @@ class ParticleBox():
     def step(self, dt):
         self.t += dt
         # print('%.1fs' % self.t)
-        # TODO: add particle interactions
-        for particle in self.particle_list:
-            if particle.position[0] < particle.radius + self.bounds[0]:
-                particle.position[0] = particle.radius + self.bounds[0]
-                particle.velocity[0] *= -1
-            if particle.position[0] > -particle.radius + self.bounds[1]:
-                particle.position[0] = -particle.radius + self.bounds[1]
-                particle.velocity[0] *= -1
-            if particle.position[1] < particle.radius + self.bounds[2]:
-                particle.position[1] = particle.radius + self.bounds[2]
-                particle.velocity[1] *= -1
-            if particle.position[1] > -particle.radius + self.bounds[3]:
-                particle.position[1] = -particle.radius + self.bounds[3]
-                particle.velocity[1] *= -1
+        # for p1 in particle_list:
+        #     for p2 in particle_list:
+        #         if p1
 
-            particle.position += dt*particle.velocity
+        for particle in self.particle_list:
+            if particle.state[0] < particle.radius + self.bounds[0]:
+                particle.state[0] = particle.radius + self.bounds[0]
+                particle.state[2] *= -1
+            if particle.state[0] > -particle.radius + self.bounds[1]:
+                particle.state[0] = -particle.radius + self.bounds[1]
+                particle.state[2] *= -1
+            if particle.state[1] < particle.radius + self.bounds[2]:
+                particle.state[1] = particle.radius + self.bounds[2]
+                particle.state[3] *= -1
+            if particle.state[1] > -particle.radius + self.bounds[3]:
+                particle.state[1] = -particle.radius + self.bounds[3]
+                particle.state[3] *= -1
+
+            particle.state[:2] += dt*particle.state[2:]
 
 
 # Set up figure
@@ -181,15 +150,11 @@ def animate(i):
     global my_box, dt
     my_box.step(dt)
     
-    particle_pos_x = [particle.position[0] for particle in my_box.particle_list]
-    particle_pos_y = [particle.position[1] for particle in my_box.particle_list]
+    particle_pos_x = [particle.state[0] for particle in my_box.particle_list]
+    particle_pos_y = [particle.state[1] for particle in my_box.particle_list]
     
     particle_pos.set_data(particle_pos_x, particle_pos_y)
     time_display.set_text('time = %.1fs' % my_box.t)
-    # for particle in my_box.particle_list:
-    #     particle.step(dt, my_box.bounds)
-    #     particle_pos_x.append(particle.position[0])
-    #     particle_pos_y.append(particle.position[1])
     return particle_pos, time_display
 
 anim = animation.FuncAnimation(fig, animate, frames=30, interval=10, blit=True,
