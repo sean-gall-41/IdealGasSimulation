e5f734367e1b7497f3bae7a050a5c160be69c4d7
Sean Gallogly <galloglyst149@gmail.com>
2019-09-27 18:22:43 -0500

    Added second box for multiple particle colors. Boxes do not iteract: requires fix

--- ideal_gas.py
+++ ideal_gas.py
@@ -16,17 +16,29 @@ SCALE_FACTOR = 0.75
 BOX_WIDTH = SCALE_FACTOR*(X_UPPER-X_LOWER)
 BOX_HEIGHT = SCALE_FACTOR*(Y_UPPER-Y_LOWER)
 
+def randomReal(a, b):
+    return (b - a) * np.random.random() + a
+
+class Error(Exception):
+    """Base class for other custom-defined exceptions (if the need arises)"""
+    pass
+
+class OutOfBoundsError(Error):
+    """Raised when something is out of bounds"""
+    pass
+
 class GasParticle:
     """docstring for GasParticle."""
 
     """To understand how we are going to initialize our ball object, 
     we need to understand how to plot artists in matplotlib"""
     def __init__(self, mass = PARTICLE_MASS, radius = PARTICLE_RADIUS,
-                 init_state = [0.0, 0.0, 0.0, 0.0]):
+                 color = 'b', init_state = [0.0, 0.0, 0.0, 0.0]):
         self.mass = mass
         self.radius = radius
         self.init_state = np.asarray(init_state, dtype=float)
         self.state = self.init_state.copy()
+        self.color = color
 
     def __eq__(self, other):
         return True if self.mass == other.mass and self.radius == other.radius and np.array_equal(self.state, other.state) else False
@@ -43,15 +55,26 @@ class GasParticle:
 # A particle box represents a group of N gas particles confined
 # within a box of dimensions bounds
 class ParticleBox():
-    def __init__(self, N=25, bounds=[-1.0, 1.0, -1.0, 1.0]):
+    def __init__(self, N=25, color='b', bounds=[-1.0, 1.0, -1.0, 1.0]):
         self.t = 0.0
         self.N = N
+        self.color = color # Should the box itsef even have this attribute?
         self.bounds = bounds
-        self.particle_list = np.array([GasParticle(PARTICLE_MASS, PARTICLE_RADIUS,
-                                        [-0.5*BOX_WIDTH + BOX_WIDTH*np.random.random(), 
-                                        -0.5*BOX_HEIGHT + BOX_HEIGHT*np.random.random(),
-                                        -1.0 + np.random.random(), -1.0 + np.random.random()]) 
+        self.particle_list = np.array([GasParticle(PARTICLE_MASS, PARTICLE_RADIUS, color,
+                                        [randomReal(-0.5*BOX_WIDTH, 0.5*BOX_WIDTH), 
+                                        randomReal(-0.5*BOX_HEIGHT, 0.5*BOX_HEIGHT),
+                                        randomReal(-5, 5), randomReal(-5, 5)]) 
                                         for i in np.arange(N)])
+
+    def add_particle(self, particle):
+        if particle.state[0] - particle.radius < self.bounds[0] or \
+           particle.state[0] + particle.radius > self.bounds[1] or \
+           particle.state[1] - particle.radius < self.bounds[2] or \
+           particle.state[1] + particle.radius > self.bounds[3]:
+            raise OutOfBoundsError 
+        
+        self.N += 1
+        self.particle_list.append(particle)
 
     # Steps the state of the particle box forward by time dt
     # => all particle positions and velocities are updated. 
@@ -132,37 +155,47 @@ rect = plt.Rectangle((-0.5*BOX_WIDTH-offset, -0.5*BOX_HEIGHT-offset), BOX_WIDTH+
 ax_1.add_patch(rect)
 
 np.random.seed(0)
-my_box = ParticleBox(100, [-0.5*BOX_WIDTH, 0.5*BOX_WIDTH, -0.5*BOX_HEIGHT, 0.5*BOX_HEIGHT])
+blue_box = ParticleBox(100, 'b', [-0.5*BOX_WIDTH, 0.5*BOX_WIDTH, -0.5*BOX_HEIGHT, 0.5*BOX_HEIGHT])
+red_box = ParticleBox(1, 'r', [-0.5*BOX_WIDTH, 0.5*BOX_WIDTH, -0.5*BOX_HEIGHT, 0.5*BOX_HEIGHT])
 
 #TODO: find correct conversion factor for radius to marker size (in points)
 marker_size = int(fig.dpi*2*PARTICLE_RADIUS*fig.get_figwidth()
-                  / np.diff(ax_1.get_xbound())[0])
+                / np.diff(ax_1.get_xbound())[0])
 
-particle_pos, = ax_1.plot([], [], 'bo', ms=marker_size)
-
+blue_particle_pos, = ax_1.plot([], [],'bo', markersize=marker_size)
+red_particle_pos, = ax_1.plot([], [], 'ro', markersize=marker_size)
 
 dt = 1./30
 
 def init():
-    particle_pos.set_data([], [])
+    blue_particle_pos.set_data([], [])
+    red_particle_pos.set_data([], [])
     time_display.set_text('')
-    return particle_pos, time_display
+    return blue_particle_pos, red_particle_pos, time_display
 
 def animate(i):
-    global my_box, dt
-    my_box.step(dt)
+    global blue_box, red_box, dt
+    blue_box.step(dt)
+    red_box.step(dt)
     
-    particle_pos_x = [particle.state[0] for particle in my_box.particle_list]
-    particle_pos_y = [particle.state[1] for particle in my_box.particle_list]
+    blue_particle_pos_x = [particle.state[0] for particle in blue_box.particle_list]
+    blue_particle_pos_y = [particle.state[1] for particle in blue_box.particle_list]
     
-    particle_pos.set_data(particle_pos_x, particle_pos_y)
-    time_display.set_text('time = %.1fs' % my_box.t)
-    return particle_pos, time_display
+    red_particle_pos_x = [particle.state[0] for particle in red_box.particle_list]
+    red_particle_pos_y = [particle.state[1] for particle in red_box.particle_list]
 
-anim = animation.FuncAnimation(fig, animate, frames=30, interval=5, blit=True,
-                               init_func=init)
+    blue_particle_pos.set_data(blue_particle_pos_x, blue_particle_pos_y)
+    red_particle_pos.set_data(red_particle_pos_x, red_particle_pos_y)
+    time_display.set_text('time = %.1fs' % blue_box.t)
+    return blue_particle_pos, red_particle_pos, time_display
 
-plt.show()        
+anim = animation.FuncAnimation(fig, animate, frames=30, interval=30, blit=True,
+                            init_func=init)
+
+plt.show()
+
+
+        
 
 
 
